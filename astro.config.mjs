bbe9dba1bc167d61631766c2202526322afa05c2
chulsu0506 <chulsu0506@gmail.com>
2026-08-28 08:31:11 +0900

    fix: bundle global Tailwind CSS properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://infantpolicy.infoarounds.com',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind(),
   ],
 });
